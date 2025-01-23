2d28851bcc10f67c3749bc9bb534a742ab8fe524
saneryesil <saneryesil@neutectr.com>
2025-01-23 17:24:50 +0300

    Creating the Dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import MainNavbar from "../../components/nav/MainNavbar/MainNavbar.jsx";
 import SideNavbar from "../../components/nav/SideNavbar/SideNavbar.jsx";
 import MainCard from "../../components/card/MainCard/MainCard.jsx";
+import {buildStyles, CircularProgressbarWithChildren} from "react-circular-progressbar";
 
 export default function Dashboard() {
     return (
@@ -24,15 +25,46 @@ export default function Dashboard() {
                                 <p>.</p>
                                 <p>Today</p>
                             </div>
-                            <MainCard status="Not Started" priority="Moderate"
-                                      cardBody="Buy gifts on the way and pick up cake from the bakery. (6 PM | Fresh Elements)....."
-                                      cardTitle="Attend Nischal’s Birthday Party" createdAt="20/06/2023" image="src/assets/maincard-test-react.jpg"/>
-                            <MainCard status="Not Started" priority="Moderate"
-                                      cardBody="Buy gifts on the way and pick up cake from the bakery. (6 PM | Fresh Elements)....."
-                                      cardTitle="Attend Nischal’s Birthday Party" createdAt="20/06/2023" image="src/assets/maincard-test-react.jpg"/>
-                            <MainCard status="Not Started" priority="Moderate"
-                                      cardBody="Buy gifts on the way and pick up cake from the bakery. (6 PM | Fresh Elements)....."
-                                      cardTitle="Attend Nischal’s Birthday Party" createdAt="20/06/2023" image="src/assets/maincard-test-react.jpg"/>
+                            <div className="dashboard-page-todo-cards">
+                                <MainCard status="Not Started" priority="Moderate"
+                                           cardBody="Buy gifts on the way and pick up cake from the bakery. (6 PM | Fresh Elements)....."
+                                           cardTitle="Attend Nischal’s Birthday Party" createdAt="20/06/2023"
+                                           image="src/assets/maincard-test-react.jpg"/>
+                                <MainCard status="Not Started" priority="Moderate"
+                                          cardBody="Buy gifts on the way and pick up cake from the bakery. (6 PM | Fresh Elements)....."
+                                          cardTitle="Attend Nischal’s Birthday Party" createdAt="20/06/2023"
+                                          image="src/assets/maincard-test-react.jpg"/>
+                                <MainCard status="Not Started" priority="Moderate"
+                                          cardBody="Buy gifts on the way and pick up cake from the bakery. (6 PM | Fresh Elements)....."
+                                          cardTitle="Attend Nischal’s Birthday Party" createdAt="20/06/2023"
+                                          image="src/assets/maincard-test-react.jpg"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dashboard-page-status-completed-wrapper">
+                        <div className="dashboard-page-status-card">
+                            <div className="dashboard-page-status-card-header">
+                                <img src="src/assets/add-react.svg" alt="add"/>
+                                <h3>Task Status</h3>
+                            </div>
+                            <div className="dashboard-page-status-card-body">
+                                <div className="dashboard-page-status-card-body-item">
+                                    <CircularProgressbarWithChildren value={20}   styles={buildStyles({
+                                        pathColor: "#FFB946",
+                                        textColor: "#FFB946",
+                                        trailColor: "#E0E0E0"
+                                    })}>20</CircularProgressbarWithChildren>
+                                    <h4>Completed</h4>
+                                </div>
+                                <div className="dashboard-page-status-card-body-item">
+                                    <p>20</p>
+                                    <h4>In Progress</h4>
+                                </div>
+                                <div className="dashboard-page-status-card-body-item">
+                                    <p>20</p>
+                                    <h4>Not Started</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
